05f21f657f315d774decd9b04743ee36fe22a531
Justin Doak <justinhdoak@gmail.com>
2016-11-18 15:28:47 -0500

    Added README.md with current problem statuses. Started refactoring/restructuring problem 5 solution.

---
+++ README.md
@@ -0,0 +1,10 @@
+My solutions to the Project Euler programming problems found at https://projecteuler.net/
+
+
+## Current Status
+
+**Problem 1:** Solved  
+**Problem 2:** Solved  
+**Problem 3:** Unsolved  
+**Problem 4:** Solved  
+**Problem 5:** Solved and Refactoring  
--- problem-5/problem-5.js
+++ problem-5/problem-5.js
@@ -3,29 +3,54 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?*/
 
 
+// Non-Working, in progress restructuring.
 
-function isDivisible1to10(number, limit) {
-  if (number < limit) {
-    return false;
-  }
-  for (var i = 1; i <= limit; i++) {
-    if (number % i !== 0) {
-      return false;
+function smallestDivisibleBy1toLimit(limit) {
+  var num = limit;
+  var isDivisible = false;
+
+  while (!isDivisible) {
+    for (var i = 1; i <= limit; i++) {
+      if (num % i !== 0) {
+        num++;
+        break;
+      }
+      if (num % limit === 0) {
+        return num;
+      }
     }
   }
-  return true;
 }
 
-var i = 1;
-var isDivisible = false
-var smallestDivisible;
+console.log(smallestDivisibleBy1toLimit(20));
 
-while (!isDivisible) {
-  if (isDivisible1to10(i, 20)) {
-    isDivisible = true;
-    smallestDivisible = i;
-  }
-  i++;
-}
 
-console.log(smallestDivisible);
+
+
+// INITIAL, FUNCTIONAL SOLUTION
+
+// function isDivisible1to10(number, limit) {
+//   if (number < limit) {
+//     return false;
+//   }
+//   for (var i = 1; i <= limit; i++) {
+//     if (number % i !== 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// var i = 1;
+// var isDivisible = false
+// var smallestDivisible;
+
+// while (!isDivisible) {
+//   if (isDivisible1to10(i, 20)) {
+//     isDivisible = true;
+//     smallestDivisible = i;
+//   }
+//   i++;
+// }
+
+// console.log(smallestDivisible);
